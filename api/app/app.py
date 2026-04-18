b1b86808315d6222f45ce784a9f2a4345f6c3df2
chrisustomus_boimau <chrisboimau@gmail.com>
2026-04-18 18:58:26 +0700

    change cors fonfig

--- api/app/app.py
+++ api/app/app.py
@@ -45,7 +45,7 @@ app = FastAPI(lifespan=lifespan)
 # ==========================================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://galeri-sophilia-tiketing-system-1gf1ftzjw.vercel.app/",], 
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","galeri-sophilia-tiketing-system.vercel.app/",], 
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"], 
